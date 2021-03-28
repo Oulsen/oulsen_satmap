c18cb848a959c42b4ac8aa2abde43d182181d4d5
Oulsen <47483877+Oulsen@users.noreply.github.com>
2021-03-28 23:21:22 +0200

    Init

---
+++ oulsen_satmap/client.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+    while true do
+		Citizen.Wait(1)
+		if IsPedOnFoot(GetPlayerPed(-1)) then 
+			SetRadarZoom(1100)
+		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
+			SetRadarZoom(1100)
+		end
+    end
+end)
---
+++ oulsen_satmap/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Oulsen'
+description 'High res sat map with postals'
+
+client_script 'client.lua'
